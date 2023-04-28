983862cb5a8b86ce7c0235e6fe16f656972af524
Danil <fomindanjj@rambler.ru>
2023-04-28 23:33:53 +0700

    first commit

---
+++ bruter.py
@@ -0,0 +1,18 @@
+import sys
+import argparse
+import 
+
+parser = argparse.ArgumentParser()
+
+def extend_words(word):
+    
+def get_words(resume=None):
+    
+
+if __name__ == '__main__':
+    parser.add_argument('-u', type=str, required=True, help='URL of victim')    
+    parser.add_argument('-t', type=int, default=1, required=False, help='Number of threads')
+    args = parser.parse_args()  
+    
+    target_url = args.u
+    
